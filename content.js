1e4d0f2168bf71a87ddaf22c8054e23aa8fcbe1d
rnambs <rnambiar@seas.upenn.edu>
2025-02-20 20:25:23 -0500

    paywall remover

---
+++ content.js
@@ -0,0 +1,11 @@
+const style = document.createElement("style");
+style.innerHTML = `
+    .modal_modal-window-container__59UFY {
+        display: none !important;
+    }
+    body {
+        overflow: auto !important;
+    }
+`;
+document.head.appendChild(style);
+console.log("✅ hidden");
